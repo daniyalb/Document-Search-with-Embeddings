b87f27f8d792541d603880409eaccf9e958c2dc7
Daniyal Bokhari <daniyalcraft123@gmail.com>
2024-08-28 01:05:11 -0400

    use database env

--- backend/server.js
+++ backend/server.js
@@ -22,11 +22,11 @@ const upload = multer({ storage: storage });
 
 // Set up PostgreSQL connection
 const pool = new Pool({
-  // user: process.env.DATABASE_USERNAME,
-  host: "localhost",
-  database: "postgres",
-  // password: process.env.DATABASE_PASSWORD,
-  port: 5432,
+  user: process.env.DATABASE_USER,
+  host: process.env.DATABASE_HOST,
+  database: process.env.DATABASE_NAME,
+  password: process.env.DATABASE_PASSWORD,
+  port: process.env.DATABASE_PORT,
 });
 
 app.post(
